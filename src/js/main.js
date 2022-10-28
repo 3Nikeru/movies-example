a2aed835fc4fa1ad735ec96e13ff4d2f582ea382
Alina Nikeru <alina.nikeru@gmail.com>
2022-10-28 22:03:29 +0300

    upcoming movies

--- src/js/main.js
+++ src/js/main.js
@@ -43,3 +43,25 @@ fetch(generateUrls('/movie/top_rated'))
 
     document.querySelector('.top_rated').innerHTML = movies;
 })
+
+//Upcoming
+fetch(generateUrls('/movie/upcoming'))
+.then(res => res.json())
+.then(data => {
+    console.log('data: ',  data);
+    const movies = data.results.map(movie =>(`
+    <div class="movie">
+    <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="${movie.original_title}" />
+        <div class="title">
+        ${movie.original_title}
+        </div>
+        <div class="date">${movie.release_date}</div>
+        <div class="popularity">${Math.round(movie.popularity)}</div>
+    </div>
+    `
+    
+    
+    )).join('');
+
+    document.querySelector('.upcoming').innerHTML = movies;
+})
