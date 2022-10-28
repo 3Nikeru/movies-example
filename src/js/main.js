c9e6a13b72eec50db09923897e4af19ec5d5aaeb
Alina Nikeru <alina.nikeru@gmail.com>
2022-10-28 21:53:39 +0300

    top rated movies

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,24 @@ fetch(generateUrls('/movie/popular'))
     document.querySelector('.popular').innerHTML = movies;
 })
 
+//Top Rated
+fetch(generateUrls('/movie/top_rated'))
+.then(res => res.json())
+.then(data => {
+    console.log('data: ',  data);
+    const movies = data.results.map(movie =>(`
+    <div class="movie">
+    <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="${movie.original_title}" />
+        <div class="title">
+        ${movie.original_title}
+        </div>
+        <div class="date">${movie.release_date}</div>
+        <div class="popularity">${Math.round(movie.popularity)}</div>
+    </div>
+    `
+    
+    
+    )).join('');
+
+    document.querySelector('.top_rated').innerHTML = movies;
+})
